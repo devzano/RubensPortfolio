0178dc5de4c26f21a71f2c70e06b2c8b1747e949
Ruben Manzano <rubenizag@gmail.com>
2023-09-14 12:08:16 -0400

    Add files via upload
    Beta Signup Form

---
+++ src/BetaSignupForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './BetaSignupForm.css';
+
+function BetaSignupForm() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        
+        try {
+            const response = await fetch('/api/sendMail', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ firstName, lastName, email })
+            });
+
+            if (response.status === 200) {
+                setMessage('Thank you for signing up! We will send you a invititaion shortly.');
+            } else {
+                setMessage('There was an error signing up. Please try again later.');
+            }
+        } catch (error) {
+            setMessage('There was an error signing up. Please try again later.');
+        }
+    };
+
+    return (
+        <div>
+            <h2>Sign up for Beta Testing</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>First Name:</label>
+                    <input type="text" value={firstName} onChange={e => setName(e.target.value)} required />
+                </div>
+<div>
+                    <label>Last Name:</label>
+                    <input type="text" value={lastName} onChange={e => setName(e.target.value)} required />
+                </div>
+                <div>
+                    <label>Email:</label>
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+                </div>
+                <button type="submit">Sign Up</button>
+            </form>
+            {message && <p>{message}</p>}
+        </div>
+    );
+}
+
+export default BetaSignupForm;

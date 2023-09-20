86175b58a8991b5bb5ea6bd9af91399f6597a175
Ruben <rubenizag@aol.com>
2023-09-20 10:25:40 -0400

    removed particles on mobile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { isMobile } from 'react-device-detect';
 import Portfolio from './Portfolio';
 import DynamicBackground from './DynamicBackground';
 import './App.css';
@@ -6,7 +7,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <DynamicBackground/>
+      {isMobile ? (
+        // Render app background for mobile
+        <div className="mobile"></div>
+      ) : (
+        // Render dynamic background for desktop
+        <DynamicBackground />
+      )}
       <Portfolio/>
     </div>
   );

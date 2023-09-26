8ad43ae59500a0937a2161c977eb632b675ab48f
Ruben <rubenizag@aol.com>
2023-09-25 20:25:32 -0400

    added sublinks

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -10,56 +10,50 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleClickOutside = (ev) => {
-      if (navbarRef.current && !navbarRef.current.contains(ev.target)) {
+      if (navbarRef.current && ! navbarRef.current.contains(ev.target)) {
         setIsOpen(false);
         setWebDropdownOpen(false);
         setMobileDropdownOpen(false);
       }
     };
     document.addEventListener('mousedown', handleClickOutside);
-    return () => {
+    return() => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
 
   return (
     <div className="navbar-container" ref={navbarRef}>
-        {isOpen ? (
-            <div className="navbar">
-                <div className="navbar-list">
-                    <Link className="navbar-link" to="/" onClick={() => setIsOpen(false)}>Home</Link>
-                    <div
-                        onMouseEnter={() => setWebDropdownOpen(true)}
-                        onMouseLeave={() => setWebDropdownOpen(false)}
-                    >
-                        <span className="navbar-link">Web</span>
-                        {isWebDropdownOpen && (
-                            <div className="dropdown">
-                                <Link className="navbar-link" to="/watchlistr-web" onClick={() => setIsOpen(false)}>Watchlistr</Link>
-                            </div>
-                        )}
-                    </div>
-                    <div
-                        onMouseEnter={() => setMobileDropdownOpen(true)}
-                        onMouseLeave={() => setMobileDropdownOpen(false)}
-                    >
-                        <span className="navbar-link">iOS</span>
-                        {isMobileDropdownOpen && (
-                            <div className="dropdown">
-                                <Link className="navbar-link" to="/reciperealm" onClick={() => setIsOpen(false)}>RecipeRealm</Link>
-                                <Link className="navbar-link" to="/watchlistr-ios" onClick={() => setIsOpen(false)}>Watchlistr</Link>
-                            </div>
-                        )}
-                    </div>
+      {isOpen ? (
+        <div className="navbar">
+          <div className="navbar-list">
+            <Link className="navbar-link" to="/" onClick={() => setIsOpen(false)}>Home</Link>
+            <div onMouseEnter={() => setWebDropdownOpen(true)} onMouseLeave={() => setWebDropdownOpen(false)}>
+              <span className="navbar-link">Web</span>
+              {isWebDropdownOpen && (
+                <div className="dropdown">
+                  <Link className="navbar-link" to="/watchlistr-web" onClick={() => setIsOpen(false)}>Watchlistr</Link>
                 </div>
+              )}
             </div>
-        ) : (
-            <div className="navbar-icon" onClick={() => setIsOpen(!isOpen)}>
-                &#9776;
+            <div onMouseEnter={() => setMobileDropdownOpen(true)} onMouseLeave={() => setMobileDropdownOpen(false)}>
+              <span className="navbar-link">iOS</span>
+              {isMobileDropdownOpen && (
+                <div className="dropdown">
+                  <Link className="navbar-link" to="/reciperealm" onClick={() => setIsOpen(false)}>RecipeRealm</Link>
+                  <Link className="navbar-link" to="/watchlistr-ios" onClick={() => setIsOpen(false)}>Watchlistr</Link>
+                </div>
+              )}
             </div>
-        )}
+          </div>
+        </div>
+      ) : (
+        <div className="navbar-icon" onClick={() => setIsOpen(!isOpen)}>
+          &#9776;
+        </div>
+      )}
     </div>
-);
+  );
 };
 
 export default Navbar;

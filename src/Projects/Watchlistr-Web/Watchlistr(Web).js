34da15a9dc50ed0335de420184ace6779ecb2753
Ruben <rubenizag@aol.com>
2024-08-06 19:31:55 -0400

    watchlistr update

--- src/Projects/Watchlistr-Web/Watchlistr(Web).js
+++ src/Projects/Watchlistr-Web/Watchlistr(Web).js
@@ -1,22 +1,25 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../Portfolio/Portfolio.css';
 import '../../Projects/Projects.css';
-import githubLogo from '../../Projects/CodingLogos/github(light).png'
-import reactLogo from '../../Projects/CodingLogos/react.png'
-import vsLogo from '../../Projects/CodingLogos/visualstudio.png'
-import jsLogo from '../../Projects/CodingLogos/javascript.png'
-import mysqlLogo from '../../Projects/CodingLogos/mysql.png';
+import githubLogo from '../../Projects/CodingLogos/github(light).png';
+import reactLogo from '../../Projects/CodingLogos/react.png';
+import viteLogo from '../../Projects/CodingLogos/vite.png';
+import vsLogo from '../../Projects/CodingLogos/visualstudio.png';
+import jsLogo from '../../Projects/CodingLogos/javascript.png';
+
+import LoginPage from '../Watchlistr-Web/Screenshots/WatchlistrWeb(LoginPage).png';
+import SignupPage from '../Watchlistr-Web/Screenshots/WatchlistrWeb(SignupPage).png';
+import HomePage from '../Watchlistr-Web/Screenshots/WatchlistrWeb(HomePage).png';
+import MoviesPage from '../Watchlistr-Web/Screenshots/WatchlistrWeb(MoviesPage).png';
+import TVShowsPage from '../Watchlistr-Web/Screenshots/WatchlistrWeb(TVShowsPage).png';
+
 const WatchlistrWeb = () => {
   const WatchlistrWebScreens = [
-    "https://github.com/devzano/Watchlistr-Web/raw/main/React/src/styles/Screenshots/LoginPage.png?raw=true",
-    "https://github.com/devzano/Watchlistr-Web/raw/main/React/src/styles/Screenshots/SignupPage.png?raw=true",
-    "https://github.com/devzano/Watchlistr-Web/raw/main/React/src/styles/Screenshots/Upcoming.png?raw=true",
-    "https://github.com/devzano/Watchlistr-Web/raw/main/React/src/styles/Screenshots/PopularMovies.png?raw=true",
-    "https://github.com/devzano/Watchlistr-Web/raw/main/React/src/styles/Screenshots/TopRatedMovies.png?raw=true",
-    "https://github.com/devzano/Watchlistr-Web/raw/main/React/src/styles/Screenshots/PopularTVShows.png?raw=true",
-    "https://github.com/devzano/Watchlistr-Web/raw/main/React/src/styles/Screenshots/AiringToday.png?raw=true",
-    "https://github.com/devzano/Watchlistr-Web/raw/main/React/src/styles/Screenshots/TopRatedTVShows.png?raw=true",
-    "https://github.com/devzano/Watchlistr-Web/raw/main/React/src/styles/Screenshots/Watchlist.png?raw=true",
+    LoginPage,
+    SignupPage,
+    HomePage,
+    MoviesPage,
+    TVShowsPage
   ];
 
   const [watchlistrIndex, setWatchlistrIndex] = useState(0);
@@ -24,37 +27,45 @@ const WatchlistrWeb = () => {
     const interval = setInterval(() => {
       setWatchlistrIndex((prevIndex) => (prevIndex + 1) % WatchlistrWebScreens.length);
     }, 4000);
-    return() => clearInterval(interval);
+    return () => clearInterval(interval);
   }, [WatchlistrWebScreens.length]);
 
   const builtWithLogos = [
     vsLogo,
     jsLogo,
     reactLogo,
-    mysqlLogo,
+    viteLogo,
     githubLogo
   ];
 
   return (
     <div className="coding-background">
       <div className="centered-content">
-      <h1 className="title">
-          <a href="https://watchlistr-web.vercel.app" target="_blank" rel="noopener noreferrer" className="section-title">Watchlistr</a>
-      </h1>
+        <h1 className="title">
+          <a href="https://watchlistrweb.vercel.app" target="_blank" rel="noopener noreferrer" className="section-title">Watchlistr</a>
+        </h1>
         <div className="project">
           <div className="sections-container">
             <div className="section">
               <div className="project-image-container">
-                <img src={WatchlistrWebScreens[watchlistrIndex]} alt={`Project ${watchlistrIndex + 1}`} className="project-image" style={{width: '100%', maxHeight: '500px'}}/>
+                <img src={WatchlistrWebScreens[watchlistrIndex]} alt={`Project ${watchlistrIndex + 1}`} className="project-image" style={{ width: '100%', maxHeight: '500px' }} />
               </div>
               <p className="project-description">
-              Watchlistr represents the beginning of my exciting journey into the world of project development. It's a dynamic website that makes it incredibly easy for users to create personal accounts and curate their own movie and TV show watchlists. You can explore upcoming releases, popular picks, and top-rated media. Watchlistr's primary goal is to empower you to collect and save all those must-see titles for future viewing pleasure.
+                Embarking on my journey into project development, I created Watchlistr as a platform to enhance your entertainment experience. Initially, it was only a website, and I have now seamlessly integrated it with my <a href="https://www.rubenmanzano.com/watchlistr-ios" target="_blank" rel="noopener noreferrer">iOS app</a>, allowing users to enjoy a unified experience. You can easily login or create an account on either platform.
+              </p>
+
+              <p className="project-description">
+                Watchlistr makes it simple for you to create personal accounts and curate your own Movie and TV Show watchlists. Discover upcoming releases, explore popular picks, and check out top-rated media, all in one place.
+              </p>
+
+              <p className="project-description">
+                My mission is to empower you to collect and save all those must-see and classic titles for your future viewing pleasure. Join today and never miss out on the media you love!
               </p>
               <div className="logo-container">
                 {builtWithLogos.map((logo, index) => (
                   <span key={index}>
                     {index === builtWithLogos.length - 1 ? (
-                      <a href="https://github.com/devzano/Watchlistr-Web" target="_blank" rel="noopener noreferrer">
+                      <a href="https://github.com/devzano/" target="_blank" rel="noopener noreferrer">
                         <img src={logo} alt={`Logo ${index + 1}`} className="logo" />
                       </a>
                     ) : (

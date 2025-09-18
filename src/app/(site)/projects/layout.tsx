f627dee4a8d0d0d945baa708cdc3de7edae7c738
Ruben <rubenizag@gmail.com>
2025-09-18 07:22:27 -0400

    final

--- src/app/(site)/projects/layout.tsx
+++ src/app/(site)/projects/layout.tsx
@@ -1,12 +1,7 @@
 // app/(site)/projects/layout.tsx
 export const metadata = { title: "All Projects" };
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-  modal: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode; }) {
   return (
     <>
       {children}

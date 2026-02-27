04ab76c1432b9fcf54874826e89f2f559c7ba66d
Ruben <rubenizag@gmail.com>
2026-02-27 14:18:45 -0500

    reroute

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true, // This disables Vercel's processing for ALL images globally
+    unoptimized: true,
+  },
+  async redirects() {
+    return [
+      {
+        // Matches the old "manzanos-popshop/terms" route
+        source: "/manzanos-popshop/terms",
+        destination: "/mps-mobile/terms",
+        permanent: true,
+      },
+      {
+        // Matches the old "manzanos-popshop/privacy" route
+        source: "/manzanos-popshop/privacy",
+        destination: "/mps-web/privacy",
+        permanent: true,
+      },
+    ];
   },
 };
 
